c7eb33b8398b6cbf5bb74477524d7593b8d64923
nahsi <nahsi@airmail.cc>
2021-01-29 20:46:29 +0300

    Dissallow default and admin policies

--- vault/nomad.tf
+++ vault/nomad.tf
@@ -1,6 +1,6 @@
 resource "vault_token_auth_backend_role" "nomad-cluster" {
   role_name              = "nomad-cluster"
-  disallowed_policies    = ["*"]
+  disallowed_policies    = ["admin", "default", "nomad-server"]
   renewable              = true
   token_period           = "259200" # 3d
   orphan                 = true
