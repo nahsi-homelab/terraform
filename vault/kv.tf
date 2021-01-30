a9339a1db9e46840a597f0b8191fa65e7851e612
nahsi <nahsi@airmail.cc>
2021-01-30 12:41:05 +0300

    Syntax fix

--- vault/kv.tf
+++ vault/kv.tf
@@ -1,9 +1,12 @@
 resource "vault_mount" "secret" {
-  path        = "secret"
-  type        = "kv-v2"
+  path = "secret"
+  type = "kv-v2"
 }
 
 resource "vault_mount" "secret_v1" {
-  path        = "secret-v1"
-  type        = "kv-v1"
+  path = "secret-v1"
+  type = "kv"
+  options = {
+    version = 1
+  }
 }
