a89fd8a59bf3c6057c16239e1e8b4d544394c7d1
nahsi <nahsi@airmail.cc>
2021-02-13 14:10:27 +0300

    Add nomad repo

--- github/homelab/repos.tf
+++ github/homelab/repos.tf
@@ -8,6 +8,16 @@ resource "github_repository" "terraform" {
   delete_branch_on_merge = true
 }
 
+resource "github_repository" "nomad" {
+  name = "nomad"
+
+  has_issues   = false
+  has_projects = false
+  has_wiki     = false
+
+  delete_branch_on_merge = true
+}
+
 resource "github_repository" "ansible" {
   name        = "ansible"
   description = "Homelabbing with Ansible"
