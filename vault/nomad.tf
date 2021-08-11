0155fc2f4dcdb5eb9c07e0150f5158055412ea2f
Anatoly Laskaris <nahsi@airmail.cc>
2021-08-11 21:36:11 +0300

    Allow unpoller policy

--- vault/nomad.tf
+++ vault/nomad.tf
@@ -1,6 +1,11 @@
 resource "vault_token_auth_backend_role" "nomad-cluster" {
-  role_name              = "nomad-cluster"
-  allowed_policies       = ["internal-certs", "public-cert", "grafana"]
+  role_name = "nomad-cluster"
+  allowed_policies = [
+    "internal-certs",
+    "public-cert",
+    "grafana",
+    "unpoller"
+  ]
   renewable              = true
   token_period           = "259200" # 3d
   orphan                 = true
