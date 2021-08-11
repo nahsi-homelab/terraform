5a1a861d00b483f0469c99876b985358ad4153e0
Anatoly Laskaris <nahsi@airmail.cc>
2021-08-11 09:08:38 +0300

    Allow grafana policy for nomad

--- vault/nomad.tf
+++ vault/nomad.tf
@@ -1,6 +1,6 @@
 resource "vault_token_auth_backend_role" "nomad-cluster" {
   role_name              = "nomad-cluster"
-  allowed_policies       = ["internal-certs", "public-cert"]
+  allowed_policies       = ["internal-certs", "public-cert", "grafana"]
   renewable              = true
   token_period           = "259200" # 3d
   orphan                 = true
