23d5f5691daacb9cad4dc42764f789f59a08afbd
Anatoly Laskaris <nahsi@airmail.cc>
2021-10-10 20:15:58 +0300

    Add dendrite

--- vault/database.tf
+++ vault/database.tf
@@ -21,25 +21,22 @@ resource "vault_database_secret_backend_connection" "postgres" {
   }
 }
 
-resource "vault_database_secret_backend_role" "linkace" {
-  backend = vault_mount.database.path
-  name    = "linkace"
-  db_name = vault_database_secret_backend_connection.postgres.name
-  creation_statements = [
-    "CREATE ROLE \"{{name}}\" WITH LOGIN PASSWORD '{{password}}' VALID UNTIL '{{expiration}}';",
-    "GRANT ALL PRIVILEGES ON DATABASE \"linkace\" to \"{{name}}\";"
-  ]
-  revocation_statements = [
-    "REVOKE ALL PRIVILEGES ON DATABASE \"linkace\" FROM \"{{name}}\";",
-    "DROP ROLE \"{{name}}\";"
-  ]
-}
-
 resource "vault_database_secret_backend_static_role" "postgres-exporter" {
   backend  = vault_mount.database.path
   name     = "postgres-exporter"
   db_name  = vault_database_secret_backend_connection.postgres.name
   username = "postgres_exporter"
+  rotation_statements = [
+    "ALTER USER \"{{name}}\" WITH PASSWORD '{{password}}';"
+  ]
+  rotation_period = "86400"
+}
+
+resource "vault_database_secret_backend_static_role" "dendrite" {
+  backend  = vault_mount.database.path
+  name     = "dendrite"
+  db_name  = vault_database_secret_backend_connection.postgres.name
+  username = "dendrite"
   rotation_statements = [
     "ALTER USER \"{{name}}\" WITH PASSWORD '{{password}}';"
   ]
