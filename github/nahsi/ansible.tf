a692bc238569f995829ae4599bb65e2ad7ddda2f
Anatoly Laskaris <nahsi@airmail.cc>
2021-11-18 09:06:55 +0300

    Add nomad repo

--- github/nahsi/ansible.tf
+++ github/nahsi/ansible.tf
@@ -4,6 +4,7 @@ resource "github_repository" "ansible-consul" {
 
   topics = [
     "consul",
+    "hashicorp",
     "ansible"
   ]
 
@@ -14,6 +15,29 @@ resource "github_repository" "ansible-consul" {
 
 resource "github_branch_protection" "ansible-consul" {
   repository_id = github_repository.ansible-consul.name
+
+  pattern          = "master"
+  enforce_admins   = true
+  allows_deletions = true
+}
+
+resource "github_repository" "ansible-nomad" {
+  name        = "ansible-nomad"
+  description = "Ansible role for HashiCorp Nomad"
+
+  topics = [
+    "nomad",
+    "hashicorp",
+    "ansible"
+  ]
+
+  allow_merge_commit     = false
+  allow_rebase_merge     = false
+  delete_branch_on_merge = true
+}
+
+resource "github_branch_protection" "ansible-nomad" {
+  repository_id = github_repository.ansible-nomad.name
 
   pattern          = "master"
   enforce_admins   = true
