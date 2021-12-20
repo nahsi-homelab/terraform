52532a6bac32be10a217fbc4f354cd16d22fd747
Anatoly Laskaris <nahsi@airmail.cc>
2021-12-20 21:57:27 +0300

    Allow exporter role

--- mongo/vault.tf
+++ mongo/vault.tf
@@ -7,6 +7,10 @@ resource "vault_database_secret_backend_connection" "mongo" {
   backend = vault_mount.database.path
   name    = "mongo"
 
+  allowed_roles = [
+    "exporter"
+  ]
+
   mongodb {
     connection_url = "mongodb://{{username}}:{{password}}@mongo.service.consul:27017/admin?ssl=false"
   }
