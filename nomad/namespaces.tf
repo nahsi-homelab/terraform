dc249f4b76e142860b5c8de9c2a469d00f2a20db
Anatoly Laskaris <nahsi@airmail.cc>
2022-01-07 16:37:12 +0300

    More namespaces

--- nomad/namespaces.tf
+++ nomad/namespaces.tf
@@ -1,4 +1,17 @@
+resource "nomad_namespace" "services" {
+  name        = "services"
+  description = "Self-hosted goodies"
+}
+
+resource "nomad_namespace" "infra" {
+  name = "infra"
+}
+
+resource "nomad_namespace" "observability" {
+  name = "observability"
+}
+
 resource "nomad_namespace" "github" {
   name        = "github"
-  description = "github runners"
+  description = "GitHub runners"
 }
