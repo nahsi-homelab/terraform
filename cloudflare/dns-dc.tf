e60c7f0517313002ccfa83af6acd90a7d7bba106
Anatoly Laskaris <nahsi@airmail.cc>
2022-01-09 12:31:05 +0300

    Add mx records

--- cloudflare/dns-dc.tf
+++ cloudflare/dns-dc.tf
@@ -6,10 +6,28 @@ resource "cloudflare_record" "syria" {
   ttl     = 1
 }
 
+resource "cloudflare_record" "mx-syria" {
+  zone_id  = var.cloudflare_zone_id
+  name     = "nahsi.dev"
+  value    = "syria.nahsi.dev"
+  type     = "MX"
+  priority = "10"
+  ttl      = 3600
+}
+
 resource "cloudflare_record" "asia" {
   zone_id = var.cloudflare_zone_id
   name    = "asia"
   value   = var.asia
   type    = "A"
   ttl     = 1
+}
+
+resource "cloudflare_record" "mx-asia" {
+  zone_id  = var.cloudflare_zone_id
+  name     = "nahsi.dev"
+  value    = "asia.nahsi.dev"
+  type     = "MX"
+  priority = "20"
+  ttl      = 3600
 }
