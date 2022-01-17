d57da1c53fe502624c5db169206e1cd02686f7c5
Anatoly Laskaris <github_me@nahsi.dev>
2022-01-17 19:36:57 +0300

    Add PTR record

--- linode/dns.tf
+++ linode/dns.tf
@@ -11,3 +11,8 @@ resource "cloudflare_record" "pontus" {
   type    = "A"
   ttl     = 1
 }
+
+resource "linode_rdns" "pontus" {
+  address = linode_instance.heraclea.ip_address
+  rdns    = "pontus.nahsi.dev"
+}
