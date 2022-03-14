27ae74e374e067dacbca1fb5bb867150b4c69511
Anatoly Laskaris <github_me@nahsi.dev>
2022-03-14 19:49:07 +0300

    Cleanup

--- vultr/firewall.tf
+++ vultr/firewall.tf
@@ -41,13 +41,3 @@ resource "vultr_firewall_rule" "wg-relay" {
   port              = "51820"
   notes             = "wireguard relay"
 }
-
-resource "vultr_firewall_rule" "wg-common" {
-  firewall_group_id = vultr_firewall_group.instances.id
-  protocol          = "udp"
-  ip_type           = "v4"
-  subnet            = "0.0.0.0"
-  subnet_size       = 0
-  port              = "52820"
-  notes             = "wireguard common"
-}
