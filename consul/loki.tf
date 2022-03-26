602c01568cda72022e3d07c6100ae12c3edd3559
Anatoly Laskaris <github_me@nahsi.dev>
2022-03-26 19:06:39 +0300

    Allow traefik to distributor connect

--- consul/loki.tf
+++ consul/loki.tf
@@ -13,3 +13,19 @@ resource "consul_config_entry" "loki-querier" {
     ]
   })
 }
+
+resource "consul_config_entry" "loki-distributor" {
+  name = "loki-distributor"
+  kind = "service-intentions"
+
+  config_json = jsonencode({
+    Sources = [
+      {
+        Action     = "allow"
+        Name       = "traefik"
+        Precedence = 9
+        Type       = "consul"
+      },
+    ]
+  })
+}
