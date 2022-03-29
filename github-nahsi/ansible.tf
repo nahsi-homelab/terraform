d7c317ac73f853014d1709151523eb18706d7d5a
Anatoly Laskaris <github_me@nahsi.dev>
2022-03-29 17:25:17 +0300

    Add vault-agent repo

--- github-nahsi/ansible.tf
+++ github-nahsi/ansible.tf
@@ -75,3 +75,30 @@ resource "github_branch_protection" "ansible-vault" {
   enforce_admins   = true
   allows_deletions = true
 }
+
+resource "github_repository" "ansible-vault-agent" {
+  name        = "ansible-vault-agent"
+  description = "Ansible role for HashiCorp Vault agent"
+
+  topics = [
+    "vault",
+    "vault-agent",
+    "hashicorp",
+    "ansible"
+  ]
+
+  has_issues = true
+
+  allow_merge_commit     = false
+  allow_rebase_merge     = false
+  delete_branch_on_merge = true
+  vulnerability_alerts   = true
+}
+
+resource "github_branch_protection" "ansible-vault-agent" {
+  repository_id = github_repository.ansible-vault-agent.name
+
+  pattern          = "master"
+  enforce_admins   = true
+  allows_deletions = true
+}
