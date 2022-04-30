86802ba5b476bf9b85773ffa6bb4ee03f9e1631c
Anatoly Laskaris <github_me@nahsi.dev>
2022-04-30 16:53:24 +0300

    Add more cidrs to allowed

--- vault/auth_approle.tf
+++ vault/auth_approle.tf
@@ -12,6 +12,8 @@ resource "vault_approle_auth_backend_role" "vault_agent" {
 
   secret_id_bound_cidrs = [
     "10.0.100.0/24",
+    "10.0.10.0/24",
+    "10.0.20.0/24",
     "10.0.30.0/24",
     "10.1.10.0/24",
     "10.2.10.0/24",
