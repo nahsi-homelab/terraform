18b013e8c5dcccd44254dc3c2e0254abbc3d7fc0
Anatoly Laskaris <github_me@nahsi.dev>
2022-05-10 11:03:26 +0300

    Use spread algorithm

--- nomad/config.tf
+++ nomad/config.tf
@@ -1,5 +1,6 @@
 resource "nomad_scheduler_config" "config" {
   memory_oversubscription_enabled = true
+  scheduler_algorithm = "spread"
   preemption_config = {
     "batch_scheduler_enabled"    = false
     "service_scheduler_enabled"  = false
