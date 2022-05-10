82cf46c36213203b1d99a25f333b574b3bf0bb7d
Anatoly Laskaris <github_me@nahsi.dev>
2022-05-10 11:04:08 +0300

    Enable preemtion for service type jobs

--- nomad/config.tf
+++ nomad/config.tf
@@ -3,7 +3,7 @@ resource "nomad_scheduler_config" "config" {
   scheduler_algorithm = "spread"
   preemption_config = {
     "batch_scheduler_enabled"    = false
-    "service_scheduler_enabled"  = false
+    "service_scheduler_enabled"  = true
     "sysbatch_scheduler_enabled" = false
     "system_scheduler_enabled"   = false
   }
