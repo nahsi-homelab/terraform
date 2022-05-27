3127bc762bfa9ccca41617f7d3087a2cfd414e19
Anatoly Laskaris <github_me@nahsi.dev>
2022-05-27 20:38:09 +0300

    Allow traefik to ruler connect

--- consul/loki.tf
+++ consul/loki.tf
@@ -30,6 +30,22 @@ resource "consul_config_entry" "loki-compactor" {
   })
 }
 
+resource "consul_config_entry" "loki-ruler" {
+  name = "loki-ruler"
+  kind = "service-intentions"
+
+  config_json = jsonencode({
+    Sources = [
+      {
+        Action     = "allow"
+        Name       = "traefik"
+        Precedence = 9
+        Type       = "consul"
+      },
+    ]
+  })
+}
+
 resource "consul_config_entry" "loki-query-frontend" {
   name = "loki-query-frontend"
   kind = "service-intentions"
